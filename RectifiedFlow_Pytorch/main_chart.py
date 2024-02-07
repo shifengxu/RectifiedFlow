6ea0702e99a0dacf9f5e652abdda51db5a90276c
CSLS48\shifeng <shifeng001@e.ntu.edu.sg>
2024-02-07 19:27:38 +0800

    main_chart.py: add function reverse_time_ode_gradident()

--- RectifiedFlow_Pytorch/main_chart.py
+++ RectifiedFlow_Pytorch/main_chart.py
@@ -1,3 +1,4 @@
+import math
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -193,14 +194,60 @@ def change_background_local():
         print(new_image_path)
     # for
 
+def reverse_time_ode_gradient():
+    def dm_grad_fn(x_0, epsilon, t):
+        grad = -(9.95 * t + 0.05) * math.exp(-4.975 * t * t - 0.05 * t) * x_0
+        exp2 = math.exp(-9.95 * t * t - 0.1 * t)
+        grad += (9.95 * t + 0.05) * exp2 / math.sqrt(1 - exp2) * epsilon
+        return grad
+
+    ts_arr = list(range(1, 1000, 1)) # todo: change step back.
+    ts_arr = [step / 1000 for step in ts_arr] # 0.01 ~ 0.999
+    ep_arr = [0.6, 0.0, -0.6]   # epsilon array
+    x0_arr = [0.7, 0.1, -0.5]   # x_0 array
+    fig = plt.figure(figsize=(20, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.tick_params('both', labelsize=24)
+    line_arr = []
+    # (0, (5, 10)): loosely dash
+    # (5, (10, 3)): long dash with offset
+    for ep, l_stype in zip(ep_arr, ['-', (0, (5, 10)), ':']):
+        for x0, l_color in zip(x0_arr, ['r', 'g', 'b']):
+            grad_arr = []
+            for ts in ts_arr:
+                gr = dm_grad_fn(x0, ep, ts)
+                grad_arr.append(gr)
+            # for
+            lbl = f"$\\epsilon$:{ep}, $y_0$:{x0}"
+            line, = ax.plot(ts_arr, grad_arr, label=lbl, linestyle=l_stype, color=l_color)
+            line_arr.append(line)
+        # for
+    # for
+    plt.gca().invert_xaxis() # reverse time ODE. So reverse X axis.
+    legend1 = ax.legend(handles=line_arr[:3], fontsize=28, loc='upper left') # multiple legend
+    plt.gca().add_artist(legend1)
+    legend2 = ax.legend(handles=line_arr[3:6], fontsize=28, loc=(0.277, 0.655))
+    plt.gca().add_artist(legend2)
+    ax.legend(handles=line_arr[6:], fontsize=28, loc='lower left')
+
+    fig.supylabel(r'    $\frac{dy_t}{dt}$', fontsize=40, rotation=0)  # make it horizontal
+    fig.supxlabel(r'reverse-time timestep $t$', fontsize=30)
+    fig.suptitle("Diffusion model reverse-time ODE on specific points", fontsize=30)
+    # plt.show()
+    f_path = './charts/fig_reverse_time_ode_grad_dm.png'
+    fig.savefig(f_path, bbox_inches='tight')
+    print(f"file saved: {f_path}")
+    plt.close()
+
 def main():
     """ entry point """
     # run_delta_plot_distribution()
     # fid_train_from_scratch_celeba()
     # merge_image_col_row()
     # gen_img_of_step_count()
-    change_background_local()
+    # change_background_local()
     # gen_img_of_vertical_text()
+    reverse_time_ode_gradient()
 
 if __name__ == '__main__':
     main()
